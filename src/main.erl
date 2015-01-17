dcef0040de9a906ec45eeb6704788140cc28fc0a
kkuc <krzysztof.kuc@gazeta.pl>
2015-01-17 10:42:02 +0100

    main file added with hello_world test

---
+++ src/main.erl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author krzysztof
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. sty 2015 10:32
+%%%-------------------------------------------------------------------
+-module(main).
+-author("krzysztof").
+
+%% API
+-export([hello/0]).
+
+hello()->
+  io:format("Hello, world!~n").
