09b6c719ebcf9f66999eff9bbe3274b27f9f92b8
Krzysztof Plachno <krzysiekplachno@gmail.com>
2015-01-19 16:27:42 +0100

    heater added

--- src/main.erl
+++ src/main.erl
@@ -10,10 +10,8 @@
 -author("krzysztof").
 
 %% API
--compile([export_all]).
+-compile([export_all, debug_info]).
 
-hello()->
-  io:format("Hello, world!~n").
 
 sleep(T)->
   receive
@@ -25,13 +23,16 @@ startSimulation()-> startSimulation(20, -10).
 startSimulation(TInner, TOuter) ->
   register(innerTempPid,spawn(?MODULE, tempInner, [TInner])),
   register(outerTempPid,spawn(?MODULE, tempOuter, [TOuter])),
-  main(0, 10).
+  register(simulParamsPid,spawn(?MODULE, simulParams, [{10, 120960, 20}])), %{ TimeU = 60 sec, Cp_Mp = 120960, TempExp = 20 C }
+  register(maxHeaterPower,spawn(?MODULE, maxHeaterPower, [500])),
+  register(heaterPower,spawn(?MODULE, heaterPower, [500, TInner])),
+main(0, 60).
 
 main(ActualTime, DeltaTime)->
   sleep(1000),% tutaj można zrobić, zamiast tempego sleepa czekanie na podanie współczynników na receivie
   K_OI = 8.80,
   Cp_Mp = 120960,
-  DeltaTemp = K_OI * differenceTemp() * DeltaTime / Cp_Mp,
+  DeltaTemp = ( K_OI * differenceTemp() + readHeaterPower() ) * DeltaTime / Cp_Mp,
   updateInnerTemp(DeltaTemp),
   NewTime = ActualTime + DeltaTime,
   io:format("Aktualny czas: ~p sekund ~n",[NewTime]),
@@ -51,6 +52,51 @@ updateInnerTemp(DeltaTemp) ->
 readOuterTemp() ->
   whereis(outerTempPid)!{read, self()},
   readInput().
+
+readHeaterPower() ->
+  whereis(heaterPower)!{read, self()},
+  readInput().
+
+heaterPower(PreviousPower, LastInnerTemp)->
+  receive
+    {read, Pid} ->
+      % to niżej to      =  Energia od grzejnika dana pokojowi - energia, o którą sumarycznie pokój się wzbogacił
+      EnergyMovedOutside = PreviousPower * readTimeU() - ((readInnerTemp()-LastInnerTemp) * readCp_Mp()), % to co upływa jest dodatnio
+      % zatem uzyskujemy w wyniku energię, która uszła na zewnątrz
+      EnergyNeeded = (readTempExp() - readInnerTemp()) * readCp_Mp(),
+      if
+        EnergyNeeded > 0 -> NewPower = max(min(EnergyNeeded + EnergyMovedOutside, readMaxHeaterPower()),0);
+        EnergyNeeded == 0 -> NewPower = PreviousPower;
+        EnergyNeeded < 0 -> NewPower = min(max(EnergyNeeded + EnergyMovedOutside, 0), readMaxHeaterPower())
+        %tak jak pierwszy case tylko tutaj Energy Needed jest ujemne, nie do końca dobre, ale i tak nie powinno dojść do tego przypadku
+      end,
+      Pid ! NewPower,
+      heaterPower(NewPower, readInnerTemp())
+  end.
+
+readMaxHeaterPower() ->
+  whereis(maxHeaterPower)!{read, self()},
+  readInput().
+
+switchHeater(Level)->
+  whereis(maxHeaterPower)!{switch, Level}.
+
+
+maxHeaterPower(MaxPower)->
+  receive
+    {switch,0} ->
+      maxHeaterPower(0);
+    {switch,1} ->
+      maxHeaterPower(500);
+    {switch,2} ->
+      maxHeaterPower(1000);
+    {switch,3} ->
+      maxHeaterPower(1500);
+    {switch,4} ->
+      maxHeaterPower(2000);
+    {read, Pid} -> Pid ! MaxPower,
+      maxHeaterPower(MaxPower)
+  end.
 
 
 
@@ -73,3 +119,30 @@ readInput()->
   receive
     All -> All
   end.
+
+simulParams(Params)->
+  receive
+    {updateTu, Tu} ->
+      simulParams(setelement(1, Params, Tu));
+    {updateCp_Mp, Cp_Mp} ->
+      simulParams(setelement(2, Params, Cp_Mp));
+    {updateTempExp, TempExp} ->
+      simulParams(setelement(3, Params, TempExp));
+    {read, Pid} -> Pid ! Params,
+      simulParams(Params)
+  end.
+
+
+% to można zoptymalizować
+readTimeU()->
+  whereis(simulParamsPid)!{read, self()},
+  element(1,readInput()).
+
+readCp_Mp()->
+  whereis(simulParamsPid)!{read, self()},
+  element(2,readInput()).
+readTempExp()->
+  whereis(simulParamsPid)!{read, self()},
+  element(3,readInput()).
+
+
