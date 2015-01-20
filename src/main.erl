d124651cdf82edcf5a20484410aeb7119b2d4a9b
Krzysztof Plachno <krzysiekplachno@gmail.com>
2015-01-20 20:21:07 +0100

    divided into files - works

--- src/main.erl
+++ src/main.erl
@@ -8,24 +8,23 @@
 %%%-------------------------------------------------------------------
 -module(main).
 -author("krzysztof").
+-import(utils, [sleep/1, readInput/0]).
+-import(outerTemp, [tempOuter/1, readOuterTemp/0]).
+-import(heater, [heaterPower/2, maxHeaterPower/1, readHeaterPower/0, readMaxHeaterPower/0, switchHeater/1]).
 
+-import(simulParams, [readTimeU/0,readTempExp/0, readCp_Mp/0 ]).
+-import(innerTemp, [updateInnerTemp/1, tempInner/1, readInnerTemp/0]).
 %% API
 -compile([export_all, debug_info]).
 
 
-sleep(T)->
-  receive
-    after T ->
-      true
-  end.
-
 startSimulation()-> startSimulation(20, -10).
 startSimulation(TInner, TOuter) ->
-  register(innerTempPid,spawn(?MODULE, tempInner, [TInner])),
-  register(outerTempPid,spawn(?MODULE, tempOuter, [TOuter])),
-  register(simulParamsPid,spawn(?MODULE, simulParams, [{10, 120960, 20}])), %{ TimeU = 60 sec, Cp_Mp = 120960, TempExp = 20 C }
-  register(maxHeaterPower,spawn(?MODULE, maxHeaterPower, [500])),
-  register(heaterPower,spawn(?MODULE, heaterPower, [500, TInner])),
+  register(innerTempPid,spawn(innerTemp, tempInner, [TInner])),
+  register(outerTempPid,spawn(outerTemp, tempOuter, [TOuter])),
+  register(simulParamsPid,spawn(simulParams, simulParams, [{10, 120960, 20}])), %{ TimeU = 60 sec, Cp_Mp = 120960, TempExp = 20 C }
+  register(maxHeaterPower,spawn(heater, maxHeaterPower, [500])),
+  register(heaterPower,spawn(heater, heaterPower, [500, TInner])),
 main(0, 60).
 
 main(ActualTime, DeltaTime)->
@@ -41,108 +40,3 @@ main(ActualTime, DeltaTime)->
 
 differenceTemp() ->
   readOuterTemp() - readInnerTemp().
-
-readInnerTemp() ->
-  whereis(innerTempPid)!{read, self()},
-  readInput().
-
-updateInnerTemp(DeltaTemp) ->
-  whereis(innerTempPid)!{update, DeltaTemp}.
-
-readOuterTemp() ->
-  whereis(outerTempPid)!{read, self()},
-  readInput().
-
-readHeaterPower() ->
-  whereis(heaterPower)!{read, self()},
-  readInput().
-
-heaterPower(PreviousPower, LastInnerTemp)->
-  receive
-    {read, Pid} ->
-      % to niżej to      =  Energia od grzejnika dana pokojowi - energia, o którą sumarycznie pokój się wzbogacił
-      EnergyMovedOutside = PreviousPower * readTimeU() - ((readInnerTemp()-LastInnerTemp) * readCp_Mp()), % to co upływa jest dodatnio
-      % zatem uzyskujemy w wyniku energię, która uszła na zewnątrz
-      EnergyNeeded = (readTempExp() - readInnerTemp()) * readCp_Mp(),
-      if
-        EnergyNeeded > 0 -> NewPower = max(min(EnergyNeeded + EnergyMovedOutside, readMaxHeaterPower()),0);
-        EnergyNeeded == 0 -> NewPower = PreviousPower;
-        EnergyNeeded < 0 -> NewPower = min(max(EnergyNeeded + EnergyMovedOutside, 0), readMaxHeaterPower())
-        %tak jak pierwszy case tylko tutaj Energy Needed jest ujemne, nie do końca dobre, ale i tak nie powinno dojść do tego przypadku
-      end,
-      Pid ! NewPower,
-      heaterPower(NewPower, readInnerTemp())
-  end.
-
-readMaxHeaterPower() ->
-  whereis(maxHeaterPower)!{read, self()},
-  readInput().
-
-switchHeater(Level)->
-  whereis(maxHeaterPower)!{switch, Level}.
-
-
-maxHeaterPower(MaxPower)->
-  receive
-    {switch,0} ->
-      maxHeaterPower(0);
-    {switch,1} ->
-      maxHeaterPower(500);
-    {switch,2} ->
-      maxHeaterPower(1000);
-    {switch,3} ->
-      maxHeaterPower(1500);
-    {switch,4} ->
-      maxHeaterPower(2000);
-    {read, Pid} -> Pid ! MaxPower,
-      maxHeaterPower(MaxPower)
-  end.
-
-
-
-
-tempInner(Temp)->
-  receive
-    {update, Delta} ->
-      tempInner(Temp+Delta);
-    {read, Pid} -> Pid ! Temp,
-      tempInner(Temp)
-  end.
-
-tempOuter(Temp)->
-  receive
-    {read, Pid} -> Pid ! Temp,
-      tempOuter(Temp)
-  end.
-
-readInput()->
-  receive
-    All -> All
-  end.
-
-simulParams(Params)->
-  receive
-    {updateTu, Tu} ->
-      simulParams(setelement(1, Params, Tu));
-    {updateCp_Mp, Cp_Mp} ->
-      simulParams(setelement(2, Params, Cp_Mp));
-    {updateTempExp, TempExp} ->
-      simulParams(setelement(3, Params, TempExp));
-    {read, Pid} -> Pid ! Params,
-      simulParams(Params)
-  end.
-
-
-% to można zoptymalizować
-readTimeU()->
-  whereis(simulParamsPid)!{read, self()},
-  element(1,readInput()).
-
-readCp_Mp()->
-  whereis(simulParamsPid)!{read, self()},
-  element(2,readInput()).
-readTempExp()->
-  whereis(simulParamsPid)!{read, self()},
-  element(3,readInput()).
-
-
